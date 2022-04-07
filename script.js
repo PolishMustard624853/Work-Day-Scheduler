8c954323499300e08196668b85734b3991c5a42b
Connor Semb <polishmustard710@gmail.com>
2022-04-06 19:59:16 -0500

    set up first local.storage function to be functional for hour8

--- script.js
+++ script.js
@@ -4,29 +4,37 @@ setInterval(function() {
     timerE1.textContent = timer.format('MMMM Do YYYY, h:mm:ss a');
 }, 1000)
 
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        // Save text in local storage
+        localStorage.setItem("time, text");
+    })
+})
 
 
 
 // Local Storage to save information entered by user
-$("#hour8 .description").val(localStorage.getItem("#hour8"));
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
