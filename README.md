01d8194a29201da89afaad2e1046762c758fbbaa
Connor Semb <polishmustard710@gmail.com>
2022-04-14 13:40:21 -0500

    Added basic README before final commit and submit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Work-Day-Scheduler
+
+This assignment is supposed to store information locally to plan out the users day, hour by hour.
+
+I can't get the local storage to function properly, But I was able to make it so the colors change accordingly to the current time and hour of the day for past / futute / present.
+
+I just really wanted to submit this assignment seeing as it is really late. I will be attempting to come back to this for re-submission.
+
+
--- script.js
+++ script.js
@@ -4,26 +4,53 @@ setInterval(function() {
     timerE1.textContent = timer.format('MMMM Do YYYY, h:mm:ss a');
 }, 1000)
 
+
 $(document).ready(function () {
     // saveBtn click listener 
     $(".saveBtn").on("click", function () {
         // Get nearby values of the description in JQuery
         let text = $(this).siblings(".description").val();
         let time = $(this).parent().attr("id");
-
-        // Save text in local storage
-        localStorage.setItem("time, text");
+        localStorage.setItem("text, time");
     })
+    function timeTracker() {
+        let timeNow = moment().hour();
+
+        //loops over time-blocks, past / present / future
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    
+    // Local Storage to save information entered by user
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    // $("#hour9 .description").val(localStorage.getItem("hour9"));
+    // $("#hour10 .description").val(localStorage.getItem("hour10"));
+    // $("#hour11 .description").val(localStorage.getItem("hour11"));
+    // $("#hour12 .description").val(localStorage.getItem("hour12"));
+    // $("#hour13 .description").val(localStorage.getItem("hour13"));
+    // $("#hour14 .description").val(localStorage.getItem("hour14"));
+    // $("#hour15 .description").val(localStorage.getItem("hour15"));
+    // $("#hour16 .description").val(localStorage.getItem("hour16"));
+    // $("#hour17 .description").val(localStorage.getItem("hour17"));
+    
+    timeTracker();
 })
-
-
-
-// Local Storage to save information entered by user
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-
-
-
-
 
 
 
